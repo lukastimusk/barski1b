fe803e72869524e9a51d5896d4108a7885ae2e66
LTimusk <lukas.timusk@ucc.on.ca>
2024-01-17 21:32:03 -0500

    Initial Commit

---
+++ barski1/barski1App.swift
@@ -0,0 +1,17 @@
+//
+//  barski1App.swift
+//  barski1
+//
+//  Created by Lukas Timusk on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct barski1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
